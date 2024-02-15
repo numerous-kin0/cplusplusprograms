9833e2ec955d82073988ecb2ac9f7bb9bc57ef9e
numerous-kin0 <122114072+numerous-kin0@users.noreply.github.com>
2024-02-15 11:01:16 +0200

    Add files via upload
    main file to tic-tac-toe program

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//Created: 29-11-2023
+//Author: Mabo Giqwa
+//Purpose: Tic-Tac-Toe game
+
+int main()
+{
+    ticTacToe game1;
+
+    game1.get_name_of_players();
+    game1.print_board();
+    game1.begin_game();
+
+    return 0;
+}
