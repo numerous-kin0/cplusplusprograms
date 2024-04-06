35138bf92fb45d20b37efedea4532831252c6ad1
Mabo Giqwa <122114072+numerous-kin0@users.noreply.github.com>
2024-04-06 08:01:03 +0200

    Update linetype.h

--- linetype.h
+++ linetype.h
@@ -8,17 +8,17 @@
 class lineType
 {
 public:
-	double get_slope() const;
-	//Postcondition: Returns the slope of a line of the form a_1x + b_1y = c_1
-	bool check_equality(const lineType&) const;
-	//Postcondition: Returns boolean value which indicates whether two equations
-	//are equal or not.
-	bool check_if_parallel(const lineType&) const;
-	//Postcondition: Returns a boolean value which indicates whether two equations 
-	//are parallel or not.
-	bool check_if_perpendicular(const lineType&) const;
-	//Postcondition: Returns a boolean value which indicates whether two equations are 
-	//perpendicular or not.
+    double get_slope() const;
+    //Postcondition: Returns the slope of a line of the form a_1x + b_1y = c_1
+    bool check_equality(const lineType&) const;
+    //Postcondition: Returns boolean value which indicates whether two equations
+    //are equal or not.
+    bool check_if_parallel(const lineType&) const;
+    //Postcondition: Returns a boolean value which indicates whether two equations 
+    //are parallel or not.
+    bool check_if_perpendicular(const lineType&) const;
+    //Postcondition: Returns a boolean value which indicates whether two equations are 
+    //perpendicular or not.
     double get_a_in_explicit_form() const;
     //Postcondition: Returns the value of A in an explicit form y = C - Ax where A = a/b
     double get_c_in_explicit_form() const;
@@ -31,7 +31,8 @@ public:
     //parallel.
     void print_intersection_of_lines(const lineType& line2);
     //Postcondition: Prints the intersection of two lines that are not parallel.
-	lineType();
+    lineType();
+    //Default constructor
     lineType(double, double, double);
     //The values for the formal parameters are represented as Ax + By = C, in the standard
     //form.
